044e5cd7477df654b99889d661031c84453277d1
AndrewDAntonio <github email address>
2020-03-29 23:42:59 -0400

    Complete

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+let drivers = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendDriver(name) {
+    drivers.push(name);
+}
+
+function destructivelyPrependDriver(name) {
+    drivers.unshift(name);
+}
+
+function destructivelyRemoveLastDriver() {
+    drivers.pop();
+}
+
+function destructivelyRemoveFirstDriver() {
+    drivers.shift();
+}
+
+function appendDriver(name) {
+    let newDrivers = [...drivers, name];
+    return newDrivers;
+}
+
+function prependDriver(name) {
+    let newerDrivers = [name, ...drivers];
+    return newerDrivers;
+}
+
+function removeLastDriver() {
+    let noLastDriver = drivers.slice(0, drivers.length - 1);
+    return noLastDriver;
+}
+
+function removeFirstDriver() {
+    let noFirstDriver = drivers.slice(1, drivers.length);
+    return noFirstDriver
+}
